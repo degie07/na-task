7be28da8b797bcd6448e91116df0ddb4fa275edd
De Gie <degie.lim@gmail.com>
2020-05-21 23:27:04 +0200

    solutions to the level-3

---
+++ level-3/assets/js/custom.js
@@ -0,0 +1,13 @@
+var aboutBox = document.querySelector('.about');
+var aboutPhoto = document.querySelector('.about__photo img');
+var aboutBoxText = document.querySelector('.about__text');
+var mobileViewport = window.matchMedia("screen and (min-width: 768px)");
+
+
+if(aboutBoxText.offsetHeight > 407) {
+    aboutBox.style.height = 'auto';
+}
+
+if (aboutBox.offsetHeight > aboutPhoto.height && mobileViewport.matches) {
+    aboutPhoto.style.padding = "2rem";
+}
---
+++ level-3/index.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+include_once 'vendor/autoload.php';
+
+$content = [
+    'box' => [
+        'title' => 'Lorem Ipsum',
+        'text' => 'Lorem ipsum dolor sit amet, consectetur adipis scing elit. Pellentesque ac nisi felis. Suspendisse ut finibus augue.',
+        'button' => 'Button',
+        'images' => array_map(function ($src) {
+            list($width) = getimagesize($src);
+            return [
+                'url' => $src,
+                'width' => $width,
+            ];
+        }, glob("assets/images/*.jpg"))
+    ]
+];
+
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('pages/index.html', $content);
